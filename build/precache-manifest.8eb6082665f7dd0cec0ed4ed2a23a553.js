18202f0987544cc71d1e2a6a0245442f9741f08e
ming0109 <72422656+ming0109@users.noreply.github.com>
2021-07-02 14:01:50 +0800

    Add files via upload

---
+++ build/precache-manifest.8eb6082665f7dd0cec0ed4ed2a23a553.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3040df619e8e79206ab07a726eb74e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68b878f34c3b5a54c162",
+    "url": "/static/css/2.1dcdecaa.chunk.css"
+  },
+  {
+    "revision": "397bf36153bd44ea938b",
+    "url": "/static/css/main.a71bfa12.chunk.css"
+  },
+  {
+    "revision": "68b878f34c3b5a54c162",
+    "url": "/static/js/2.85264593.chunk.js"
+  },
+  {
+    "revision": "397bf36153bd44ea938b",
+    "url": "/static/js/main.52b941be.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "03e121a0deed5994ac42301b2b904f05",
+    "url": "/static/media/homePic.03e121a0.png"
+  },
+  {
+    "revision": "13f4240f16cb5f785ea438d367925aa2",
+    "url": "/static/media/logo.13f4240f.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8d035eec162d4541e09349a3ac90917.js"
+  "/precache-manifest.8eb6082665f7dd0cec0ed4ed2a23a553.js"
 );
 
 self.addEventListener('message', (event) => {
